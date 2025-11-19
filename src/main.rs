7ea51db791f150a7ca99cc08afd7e67b1f3163d4
Dmitry <bushuevdv@mail.ru>
2025-11-19 17:46:02 -0300

    remove test

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,11 @@ async fn  test_start_p2p_network() -> Result<(), Box<dyn std::error::Error>> {
     
     let port = args.get(1)
         .and_then(|p| p.parse::<u16>().ok())
-        .unwrap_or(18080);
+        .unwrap();
+
+        let peer = args.get(1)
+        .and_then(|p| p.parse::<u16>().ok())
+        .unwrap();
 
     // let port = 9090;
     
@@ -30,31 +34,46 @@ async fn  test_start_p2p_network() -> Result<(), Box<dyn std::error::Error>> {
     let server = NetworkServer::new(addr, node_id.clone());
     server.start().await?;
     
-    if port != 8080 {
-        let peer_addr: SocketAddr = format!("127.0.0.1:{}", port - 1).parse()?;
-        println!("Connecting to peer at {}...", peer_addr);
-        server.connect_to_peer(peer_addr, format!("node-{}", port - 1).into_bytes()).await?;
-        sleep(Duration::from_secs(1)).await;
-    }
+    // if port != 8080 {
+    //     let peer_addr: SocketAddr = format!("127.0.0.1:{}", port - 1).parse()?;
+    //     println!("Connecting to peer at {}...", peer_addr);
+    //     server.connect_to_peer(peer_addr, format!("node-{}", port - 1).into_bytes()).await?;
+    //     sleep(Duration::from_secs(1)).await;
+    // }
     
     let server_clone = server;
     let port_clone = port;
-    tokio::spawn(async move {
-        sleep(Duration::from_secs(2)).await;
+
+    sleep(Duration::from_secs(2)).await;
         
-        loop {
-            sleep(Duration::from_secs(3)).await;
-            
-            let peer_addresses = server_clone.get_peer_addresses().await;
-            
-            for addr in peer_addresses {
-                let ping = Message::Ping;
-                if server_clone.send_message(&addr, ping).await.is_ok() {
-                    println!("[Node {}] Sent Ping to {}", port_clone, addr);
-                }
+    loop {
+        sleep(Duration::from_secs(3)).await;
+        
+        let peer_addresses = server_clone.get_peer_addresses().await;
+        
+        for addr in peer_addresses {
+            let ping = Message::Ping;
+            if server_clone.send_message(&addr, ping).await.is_ok() {
+                println!("[Node {}] Sent Ping to {}", port_clone, addr);
             }
         }
-    });
+    }
+    // tokio::spawn(async move {
+    //     sleep(Duration::from_secs(2)).await;
+        
+    //     loop {
+    //         sleep(Duration::from_secs(3)).await;
+            
+    //         let peer_addresses = server_clone.get_peer_addresses().await;
+            
+    //         for addr in peer_addresses {
+    //             let ping = Message::Ping;
+    //             if server_clone.send_message(&addr, ping).await.is_ok() {
+    //                 println!("[Node {}] Sent Ping to {}", port_clone, addr);
+    //             }
+    //         }
+    //     }
+    // });
     
     println!("Node running. Press Ctrl+C to stop.");
     
