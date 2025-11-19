970cef3877fdabbc23a653c6baa7555368cb6acd
Dmitry <bushuevdv@mail.ru>
2025-11-19 19:39:51 -0300

    add transaction

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,8 @@
 pub mod block;
 pub mod transaction;
 pub mod account;
+pub mod transaction_pool;
+
 
 pub use block::{Block, BlockHeader};
 pub use transaction::Transaction;
--- src/core/transaction.rs
+++ src/core/transaction.rs
@@ -1,5 +1,19 @@
 use serde::Serialize;
 
+use crate::core::account::Address;
+
 #[derive(Debug, Clone, Serialize)]
 pub struct Transaction {
+    inner: TransactionInner,
+    tip: u128,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum TransactionInner {
+    Transfer {
+        from: Address,
+        to: Address,
+        amount: u128,
+    },
+    ContractCall()
 }
